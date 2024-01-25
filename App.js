0c87dd7f90185dd2e8520dcb5860d6678fde4e2a
Alexis <alexisolmedocuichan@hotmail.com>
2024-01-25 17:34:25 +0000

    Login screen
    Login screen  missing switch button and funtionality

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Text style={styles.Title}>Login</Text>
+      <TextInput style={styles.Input} placeholder="Username" > </TextInput>
+
+      <TextInput style={styles.Input} placeholder="Password" > </TextInput>
+      <TouchableOpacity style={styles.LoginButton} ><Text style={styles.ButtonText}>Login</Text></TouchableOpacity>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -17,4 +22,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  Input:{
+    width:'100%',
+    height: 40,
+    borderColor: 'balck',
+    borderWidth: 1,
+    marginBottom:16,
+    padding:8, 
+  },
+  LoginButton:{
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius : 5,
+
+  },
+  ButtonText:{
+     color:'white',
+     fontSize: 16,
+     textAlign: "center"
+  },
+  
+}); 
+ 
