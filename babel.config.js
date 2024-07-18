5f8d3d90fa939e57fe4bc0ef42f12557ef6927a9
Alexis <alexisolmedocuichan@hotmail.com>
2024-07-18 18:35:11 +0300

    implementation unit test

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,12 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
   };
+
+};
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-typescript',
+  ],
 };
